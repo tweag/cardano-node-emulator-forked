31cf8556a5c025a5301aad7ef6f316bdc39c77b5
mmontin <mathieu.montin@moduscreate.com>
2025-03-31 15:58:13 +0200

    improving dependencies

--- flake.nix
+++ flake.nix
@@ -1,57 +1,125 @@
-# This file is part of the IOGX template and is documented at the link below:
-# https://www.github.com/input-output-hk/iogx#31-flakenix
-
 {
-  description = "Cardano Node Emulator";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs = {
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hpkgs = pkgs.haskell.packages.ghc96;
 
-    iogx = {
-      url = "github:input-output-hk/iogx";
-      inputs.hackage.follows = "hackage";
-      inputs.CHaP.follows = "CHaP";
-      inputs.haskell-nix.follows = "haskell-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+        ## We change the way 'blst' is built so that it takes into
+        ## account the current architecture of the processor. This
+        ## is due to a bug where older processors (>= 10 years)
+        ## would not be supported. This should not change anything
+        ## on newer machines. This could be revised in the future.
+        blst-portable = pkgs.blst.overrideAttrs (_: _: {
+          buildPhase = ''
+            runHook preBuild
+            ./build.sh -shared -D__BLST_PORTABLE__ ${
+              pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isWindows
+              "flavour=mingw64"
+            }
+            runHook postBuild
+          '';
+        });
 
-    nixpkgs.follows = "haskell-nix/nixpkgs";
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt-classic.enable = true;
+            ormolu.enable = true;
+            cabal-fmt.enable = true;
+          };
+          tools = {
+            ## This setting specifies which tools to use in the `pre-commit`
+            ## hooks. Since we take our tools (`nixfmt`, `ormolu`, `hpack`) from
+            ## `nixpkgs`, then we can simply make sure that
+            ## `pre-commit-hooks.nix`'s `nixpkgs` input follows ours, so there
+            ## is nothing to see here.
+            ##
+            ## NOTE: Configuring `hpack` here would have no effect. See
+            ## https://github.com/cachix/pre-commit-hooks.nix/issues/255
+            ## for more information.
+          };
+        };
+      in {
+        formatter = pkgs.nixfmt-classic;
 
-    hackage = {
-      url = "github:input-output-hk/hackage.nix";
-      flake = false;
-    };
+        devShells = let
+          ## The minimal dependency set to build the project with `cabal`.
+          buildInputs = [
+            blst-portable
+            pkgs.pkg-config
+            pkgs.glibcLocales
+            pkgs.zlib
+            pkgs.libsodium
+            pkgs.secp256k1
+            hpkgs.ghc
+            hpkgs.cabal-install
+          ];
 
-    CHaP = {
-      url = "github:IntersectMBO/cardano-haskell-packages?ref=repo";
-      flake = false;
-    };
+          ## Folders in which to find ".so" files
+          LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath [
+            pkgs.xz
+            pkgs.zlib
+            pkgs.openssl_3_4
+            pkgs.postgresql # For cardano-node-emulator
+            pkgs.openldap # For freer-extras‽
+          ];
 
-    haskell-nix = {
-      url =
-        "github:input-output-hk/haskell.nix/1619ca9079429f37de72923f408920dfc2985766";
-      inputs.hackage.follows = "hackage";
-    };
+          LANG = "C.UTF-8";
 
-    # Used to provide the cardano-node and cardano-cli executables.
-    # cardano-node = {
-    #   url = "github:input-output-hk/cardano-node?ref=8.8.0-pre";
-    # };
+        in {
+          ci = pkgs.mkShell {
+            inherit buildInputs;
+            inherit LD_LIBRARY_PATH;
+            inherit LANG;
+          };
 
-    mithril = { url = "github:input-output-hk/mithril"; };
-  };
+          default = pkgs.mkShell {
+            buildInputs = buildInputs ++ [
+              pkgs.hpack
+              pkgs.hlint
+              hpkgs.ormolu
+              hpkgs.haskell-language-server
+            ];
 
-  outputs = inputs:
-    inputs.iogx.lib.mkFlake {
-      inherit inputs;
-      repoRoot = ./.;
-      systems = [ "x86_64-darwin" "x86_64-linux" ];
-      outputs = import ./nix/outputs.nix;
-    };
+            inherit LD_LIBRARY_PATH;
+            inherit LANG;
+
+            # In addition to the pre-commit hooks, this redefines a cabal
+            # command that gets rid of annoying "Writing: .....*.html" output
+            # when running cabal test.
+            shellHook = pre-commit.shellHook + ''
+              function cabal() {
+                    if [ "$1" != "test" ]; then
+                      command cabal "$@"
+                    else
+                      command cabal --test-option=--color=always "$@" | grep -vE --color=never "^Writing:.*html$"
+                    fi
+              }
+              export -f cabal
+            '';
+          };
+        };
+
+        checks = { inherit pre-commit; };
+      });
 
   nixConfig = {
-    extra-substituters = [ "https://cache.iog.io" ];
-    extra-trusted-public-keys =
-      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    extra-trusted-substituters = [
+      "https://tweag-cooked-validators.cachix.org/"
+      "https://pre-commit-hooks.cachix.org/"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "tweag-cooked-validators.cachix.org-1:g1TP7YtXjkBGXP/VbSTGBOGONSzdfzYwNJM27bn8pik="
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
     allow-import-from-derivation = true;
     accept-flake-config = true;
   };
